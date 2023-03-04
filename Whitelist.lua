ae719f2fb2a0e73344db317a1db7409d9f8480f4
ElbolisHub <125166857+ElbolisHub@users.noreply.github.com>
2023-03-03 17:49:30 -0800

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,6 +1,6 @@
 return {
 "07642c65-73ff-489c-9fe2-db2d9d840365",
 "bac010bc-fa4b-45af-9af6-4ae1f1f5b6b1",
-"44914465-9e8e-4bf8-8ed7-93076fd98bdb',
+"44914465-9e8e-4bf8-8ed7-93076fd98bdb",
 "0ce4eff5-7d17-428a-8256-5237ac57f317",
 }
